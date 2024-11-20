0fce4f706efeb13193911d60468bbecb1b823022
saksham <x23266724@student.ncirl.ie>
2024-11-20 11:47:59 +0000

    __init__.py imported packages

--- cintegration/__init__.py
+++ cintegration/__init__.py
@@ -1,1 +1,11 @@
-'''DEMO DOCUMENT STRING'''
+import os
+
+from django.core.asgi import get_asgi_application
+
+from pathlib import Path
+
+
+from django.core.wsgi import get_wsgi_application
+
+from django.contrib import admin
+from django.urls import path
